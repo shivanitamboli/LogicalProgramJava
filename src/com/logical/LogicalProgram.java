804c28b2ee1290d20720151d3962f7e7cac84d64
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-22 18:01:06 +0530

    Added:Coupon number

--- src/com/logical/LogicalProgram.java
+++ src/com/logical/LogicalProgram.java
@@ -2,31 +2,28 @@ package com.logical;
 import java.util.Scanner;
 
 public class LogicalProgram {
-
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number :");
-        int num = sc.nextInt();
-        primeNUmber(num);
-        sc.close();
-    }
-    /* Method use:
-     * the number is divided greater than 1 and divided by 1 or itself is referred to as the Prime
-     * number. 0 and 1 are not counted as prime numbers.
-     */
-    public static void primeNUmber(int num){
-        boolean flag = false;
-        for (int i = 2; i <= num / 2; ++i) {
-            if (num % i == 0) {
-                flag = true;
-                break;
+        System.out.println("Enter coupon number");
+        char n = sc.next().charAt(0);
+
+        boolean[] isCollected = new boolean[n];
+        int count = 0;
+        int distinct = 0;
+        /*
+         * This loop repeatedly chose a random number and
+         * check whether it is a new one
+         */
+        while (distinct < n) {
+            int value = (int) (Math.random() * n);
+            count++;
+            if (!isCollected[value]) {
+                distinct++;
+                isCollected[value] = true;
             }
         }
-
-        if (!flag)
-            System.out.println(num + " is a prime number.");
-        else
-            System.out.println(num + " is not a prime number.");
+        /* print the total number of cards collected */
+        System.out.println("Coupon code is : " + count);
 	}
 
 }
